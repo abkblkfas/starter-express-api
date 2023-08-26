dc8172dca4fc6afba1c796db16aeb1f4151f6257
ehabatef205 <ehabmahmoud205@gmail.com>
2023-08-26 10:58:52 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import express from "express";
+import cors from "cors";
+import * as paypal from "./paypal-api.js";
+const { PORT = 7011 } = process.env;
+
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json({ limit: "50mb" }));
+
+app.post("/my-server/create-paypal-order", async (req, res) => {
+
+  const order = await paypal.createOrder(req.body);
+  res.json(order);
+});
+
+app.post("/my-server/capture-paypal-order", async (req, res) => {
+  const { orderID } = req.body;
+
+  const captureData = await paypal.capturePayment(orderID);
+  console.log(captureData.status);
+  res.json(captureData);
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}/`);
+});
